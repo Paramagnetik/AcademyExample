bd79b3802087c56e3d40a4140cd94fc1bd73b3b0
Paramagnetik <Smirnow-family@mail.ru>
2025-12-09 00:02:31 +0300

    BrowserRouter revert

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './fix-mobile.css';
 const rootElement = document.getElementById('root');
@@ -9,7 +9,7 @@ const root = createRoot(rootElement);
 const basename = process.env.NODE_ENV === 'production' ? '/AcademyExample' : '/';
 
 root.render(
-  <HashRouter basename={basename}>
+  <BrowserRouter basename={basename}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
