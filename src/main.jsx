492cf6d06668fbb0c8acdf6651ab0eba4246345d
Paramagnetik <Smirnow-family@mail.ru>
2025-12-08 22:19:03 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/AcademyExample">
     <App />
   </BrowserRouter>
 );
